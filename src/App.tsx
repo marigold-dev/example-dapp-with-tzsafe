fa032bc26de088afd7d6d29c0e650c87904731d4
Quentin Burg <quentin@marigold.dev>
2024-03-25 17:55:59 +0100

    :sparkles: manage challenges from poe

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import "./App.css";
 import { TezosToolkit } from "@taquito/taquito";
 import { RPC_URL } from "./config";
 import { AccountInfo, NetworkType } from "@airgap/beacon-sdk";
 import { BeaconWallet } from "./taquitoWallet";
-import { getProposalIdOfCreatedProposal } from "./utils";
+import { getProposalIdOfCreatedProposal, omit } from "./utils";
+import { stringToBytes } from "@taquito/utils";
 
 const Tezos = new TezosToolkit(RPC_URL);
 const beaconWallet = new BeaconWallet({
@@ -21,21 +22,90 @@ function App() {
   const [txHash, setTxHash] = useState<undefined | string>();
   const [waitingId, setWaitingId] = useState<undefined | string>();
   const [claimed, setClaimed] = useState(false);
+  const [waitingChallenges, setWaitingChallenges] = useState<
+    Record<string, string>
+  >({});
+
+  const storeWaitingChallenge = (payloadHash: string, payload: string) => {
+    const payloadEncoded = stringToBytes(payload);
+    setWaitingChallenges((old) => ({ ...old, [payloadHash]: payloadEncoded }));
+  };
+
+  const removeWaitingChallenge = useCallback(
+    (payloadHash: string) => {
+      setWaitingChallenges(omit(payloadHash, waitingChallenges));
+    },
+    [waitingChallenges]
+  );
+
+  const connectWithSimulatedProofOfEvent = async () => {
+    const operationList = await beaconWallet.requestSimulatedProofOfEvent();
+
+    console.log("Operations:", atob(operationList));
+
+    const preapply = await Tezos.rpc.preapplyOperations(
+      JSON.parse(atob(operationList))
+    );
+
+    console.log("Preapply response:", preapply);
+
+    if (
+      preapply[0].contents.every(
+        (transaction) =>
+          // @ts-expect-error - We know the type that'll be returned
+          transaction.metadata.internal_operation_results[0].result.status ===
+          "applied"
+      )
+    ) {
+      console.log(
+        "The emitted proof of event:",
+        // @ts-expect-error - We know the type that'll be returned
+        preapply[0].contents[2].metadata.internal_operation_results[2]
+      );
+      console.warn("Simulated Proof Of Event succeeded");
+      return true;
+    } else {
+      console.warn("Simulated Proof Of Event failed");
+      return false;
+    }
+  };
 
   useEffect(() => {
     beaconWallet.client.getActiveAccount().then((info) => {
       setWalletInfo(info);
-      if (!!info) {
+      if (info) {
         Tezos.setWalletProvider(beaconWallet);
       }
     });
   }, []);
 
   useEffect(() => {
+    const id = setInterval(() => {
+      Object.keys(waitingChallenges).forEach((payloadHash) => {
+        return fetch(
+          `https://api.ghostnet.tzkt.io/v1/contracts/events?contract=${walletInfo?.address}&tag=proof_of_event&payload=${waitingChallenges[payloadHash]}`
+        )
+          .then((r) => r.json())
+          .then((eventChallenges) => {
+            if (eventChallenges.length !== 0) {
+              return removeWaitingChallenge(payloadHash);
+            }
+          });
+      });
+    }, 5000);
+
+    return () => {
+      clearInterval(id);
+    };
+  }, [waitingChallenges, walletInfo?.address, removeWaitingChallenge]);
+
+  console.log(waitingChallenges);
+
+  useEffect(() => {
     if (!waitingId) return;
     const id = setInterval(() => {
       fetch(
-        `https://api.ghostnet.tzkt.io/v1/contracts/events?contract=${walletInfo?.address}&tag=resolve_proposal&payload.proposal_id=${waitingId}`,
+        `https://api.ghostnet.tzkt.io/v1/contracts/events?contract=${walletInfo?.address}&tag=resolve_proposal&payload.proposal_id=${waitingId}`
       )
         .then((r) => r.json())
         .then((v) => {
@@ -79,6 +149,24 @@ function App() {
           </button>
           <button
             style={{
+              marginRight: "8px",
+            }}
+            onClick={async () => {
+              await beaconWallet.requestPermissions({
+                network: { type: NetworkType.GHOSTNET },
+              });
+
+              const isVerified = await connectWithSimulatedProofOfEvent();
+              if (!isVerified) return;
+              beaconWallet.client.getActiveAccount().then(setWalletInfo);
+
+              Tezos.setWalletProvider(beaconWallet);
+            }}
+          >
+            Connect with Simulated Proof Of Event
+          </button>
+          <button
+            style={{
               marginLeft: "8px",
             }}
             onClick={async () => {
@@ -108,7 +196,7 @@ function App() {
                     proxy_for: undefined,
                     token_id: TOKEN_ID,
                   })
-                  .toTransferParams(),
+                  .toTransferParams()
               );
               const transaction = await contract.methodsObject
                 .claim({
@@ -122,7 +210,7 @@ function App() {
               await transaction.confirmation();
               setTxHash(transaction.opHash);
               getProposalIdOfCreatedProposal(transaction.opHash).then(
-                setWaitingId,
+                setWaitingId
               );
             }}
           >
@@ -139,7 +227,7 @@ function App() {
               console.log("Operations:", atob(operationList));
 
               const preapply = await Tezos.rpc.preapplyOperations(
-                JSON.parse(atob(operationList)),
+                JSON.parse(atob(operationList))
               );
 
               console.log("Preapply response:", preapply);
@@ -149,11 +237,14 @@ function App() {
                   (transaction) =>
                     // @ts-expect-error - We know the type that'll be returned
                     transaction.metadata.internal_operation_results[0].result
-                      .status === "applied",
+                      .status === "applied"
                 )
               ) {
-                console.log("The emitted proof of event:", preapply[0].contents[2].metadata.internal_operation_results[2]);
-                alert("Simulated Proof Of Event succeeded");                
+                console.log(
+                  "The emitted proof of event:",
+                  preapply[0].contents[2].metadata.internal_operation_results[2]
+                );
+                alert("Simulated Proof Of Event succeeded");
               } else {
                 alert("Simulated Proof Of Event failed");
               }
@@ -161,6 +252,24 @@ function App() {
           >
             Simulated Proof of Event
           </button>
+
+          <button
+            style={{
+              marginRight: "8px",
+            }}
+            onClick={async () => {
+              const payload = new Date().toISOString();
+
+              const poeResponse = await beaconWallet.requestProofOfEvent(
+                payload
+              );
+
+              storeWaitingChallenge(poeResponse.payloadHash, payload);
+            }}
+          >
+            Proof of Event
+          </button>
+
           <button
             style={{
               marginLeft: "8px",
